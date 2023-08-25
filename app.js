15718a2352c4596d0a9217814b30b4d9dc575b4d
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-08-25 03:59:24 +0300

    fix: db connection

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-import express from "express"
-import dotenv from "dotenv"
-import db from "./dataBase/connect.js"
-import userRoute from './routes/userRoutes.js'
-dotenv.config()
+import express from "express";
+import db from "./dataBase/connect.js";
+import userRoute from './routes/userRoutes.js';
 
-const app = express()
-const Port = process.env.PORT
+const app = express();
+const Port = process.env.PORT;
 
-app.use(express.json())
-app.use("/api/user", userRoute)
-
-
-
-
+app.use(express.json());
+app.use("/api/user", userRoute);
 
 app.get("/", async (req, res) => {
 	// const [data] = await db.query(
 	// 	"insert into employees values (2 ,'ahmed','doctor','electronics','b1','a@gmail','123',0)"
 	// )
-    const [data]  = await db.query("select * from employees")
-	res.send(data)
-})
+	const [data] = await db.query("select * from employees");
+	res.send(data);
+});
 
 const start = () => {
 	if (!db) {
-		console.log("ERROR : database is not connected")
-		return
+		console.log("ERROR : database is not connected");
+		return;
 	}
-	app.listen(Port, () => console.log(`app listening on port ${Port}...........`))
-}
+	app.listen(Port, () => console.log(`app listening on port ${Port}...........`));
+};
 
-start()
+start();
